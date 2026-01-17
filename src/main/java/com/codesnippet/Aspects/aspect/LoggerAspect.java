d82d98031f289c196f73845c67a71abe562ab982
Prajwal M V <mvprajwal546@gmail.com>
2026-01-17 11:58:37 +0530

    Named PointCut

--- src/main/java/com/codesnippet/Aspects/aspect/LoggerAspect.java
+++ src/main/java/com/codesnippet/Aspects/aspect/LoggerAspect.java
@@ -4,13 +4,17 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggerAspect {
 
-    @Around("execution(* com.codesnippet.Aspects.controller.*.*(..))")
+    @Pointcut("execution(* com.codesnippet.Aspects.controller.*.*(..))")
+    public void controllerPointcut() {}
+
+    @Around("controllerPointcut()")
     public Object log(ProceedingJoinPoint joinPoint) throws Throwable
     {
         System.out.println("Aspect log called");
